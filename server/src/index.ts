72a7a2b898c0fe3a58275d69392845f002f27ccb
chalscc <carlescatalancalabuig@gmail.com>
2024-01-24 11:43:37 +0100

    Configuracion servidor Express + Posgres + TypeORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,52 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { Request, Response } from "express"
+import { AppDataSource } from "./data-source"
+import { Routes } from "./routes"
+import { User } from "./entity/User"
 
-dotenv.config();
+AppDataSource.initialize().then(async () => {
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+    // create express app
+    const app = express()
+    app.use(bodyParser.json())
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next)
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+            } else if (result !== null && result !== undefined) {
+                res.json(result)
+            }
+        })
+    })
+
+    // setup express app here
+    // ...
+
+    // start express server
+    app.listen(3000)
+
+    // insert new users for test
+    await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, {
+            firstName: "Timber",
+            lastName: "Saw",
+            age: 27
+        })
+    )
+
+    await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, {
+            firstName: "Phantom",
+            lastName: "Assassin",
+            age: 24
+        })
+    )
+
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+
+}).catch(error => console.log(error))
