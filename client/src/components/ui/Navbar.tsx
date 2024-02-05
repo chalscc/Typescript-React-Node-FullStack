e4d0689cc8fca4022136454aeba91c40fe14bc11
chalscc <carlescatalancalabuig@gmail.com>
2024-02-05 11:35:41 +0100

    Cambiado texto

--- client/src/components/ui/Navbar.tsx
+++ client/src/components/ui/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
         className="navbar-brand"
         to="/"
       >
-        Sercomgas
+        Prueba técnica
       </Link>
 
       <div className="navbar-collapse">
